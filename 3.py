cb99df29fcbd09a9b21f85369416a50a6fcad699
Vasil <vasilgm@yandex.ru>
2023-05-31 21:03:07 +0300

    3

---
+++ 3.py
@@ -0,0 +1,16 @@
+n = input()
+print(n)
+n = n.split('.')
+print(n)
+n = " ".join(n)
+print(n)
+n = n.split(' ')
+print(n)
+n = set(n)
+print(n)
+print(len(n))
+
+
+# {, , , , , , , , 'sure.So', , , ,, , '
+#{, , , , , ', , , , 'So', , 'sure.', , , }
+# She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells
